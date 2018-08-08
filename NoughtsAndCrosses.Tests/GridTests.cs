23b9d9cdfc4dc011df0b11bab474715dc2f0412f
Oliver Ranson <Oliver.Ranson@firsthelpline.com>
2018-08-08 15:51:33 +0100

    Verifies grid class set postions

---
+++ NoughtsAndCrosses.Tests/GridTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+
+namespace NoughtsAndCrosses.Tests
+{
+    class GridTests
+    {
+        private static Player PlayerOne => new Player("Player One", "X");
+        private static Player PlayerTwo => new Player("Player Two", "O");
+        private Grid _grid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _grid = new Grid();
+        }
+
+        [Test]
+        public void Should_set_grid_postion_A()
+        {
+            _grid.SetNoughtOrCross("a", PlayerOne);
+
+            Assert.That(_grid.PositionA, Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_B()
+        {
+            _grid.SetNoughtOrCross("b", PlayerTwo);
+
+            Assert.That(_grid.PositionB, Is.EqualTo("O"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_C()
+        {
+            _grid.SetNoughtOrCross("c", PlayerOne);
+
+            Assert.That(_grid.PositionC, Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_D()
+        {
+            _grid.SetNoughtOrCross("d", PlayerOne);
+
+            Assert.That(_grid.PositionD, Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_E()
+        {
+            _grid.SetNoughtOrCross("e", PlayerOne);
+
+            Assert.That(_grid.PositionE, Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_F()
+        {
+            _grid.SetNoughtOrCross("f", PlayerOne);
+
+            Assert.That(_grid.PositionF, Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_G()
+        {
+            _grid.SetNoughtOrCross("g", PlayerOne);
+
+            Assert.That(_grid.PositionG, Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_H()
+        {
+            _grid.SetNoughtOrCross("h", PlayerOne);
+
+            Assert.That(_grid.PositionH, Is.EqualTo("X"));
+        }
+
+        [Test]
+        public void Should_set_grid_postion_I()
+        {
+            _grid.SetNoughtOrCross("i", PlayerOne);
+
+            Assert.That(_grid.PositionI, Is.EqualTo("X"));
+        }
+    }
+}
--- NoughtsAndCrosses.Tests/NoughtsAndCrossesTests.cs
+++ NoughtsAndCrosses.Tests/NoughtsAndCrossesTests.cs
@@ -29,7 +29,36 @@ namespace NoughtsAndCrosses.Tests
             writer.Received().WriteLine("+---+---+---+\n| X | X | c |\n+---+---+---+\n| O | O | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
             writer.Received().WriteLine("Matt, please enter your move");
             writer.Received().WriteLine("+---+---+---+\n| X | X | X |\n+---+---+---+\n| O | O | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
-            writer.Received().WriteLine($"Winner {_playerOne}!");
+            writer.Received().WriteLine("Winner Matt!");
+        }
+
+        [Test]
+        public void Player_two_wins()
+        {
+            var writer = Substitute.For<IWriter>();
+            writer.ReadLine().Returns("Matt", "Olly", "c", "a", "g", "e", "b","i");
+
+            new Game(writer).Start();
+
+            writer.Received(18).WriteLine(Arg.Any<string>());
+            writer.Received().WriteLine("Enter player one name");
+            writer.Received().WriteLine("Hello Matt");
+            writer.Received().WriteLine("Enter player two name");
+            writer.Received().WriteLine("Hello Olly");
+            writer.Received().WriteLine("+---+---+---+\n| a | b | c |\n+---+---+---+\n| d | e | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine("Matt, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| a | b | X |\n+---+---+---+\n| d | e | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine("Olly, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| O | b | X |\n+---+---+---+\n| d | e | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine("Matt, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| O | b | X |\n+---+---+---+\n| d | e | f |\n+---+---+---+\n| X | h | i |\n+---+---+---+");
+            writer.Received().WriteLine("Olly, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| O | b | X |\n+---+---+---+\n| d | O | f |\n+---+---+---+\n| X | h | i |\n+---+---+---+");
+            writer.Received().WriteLine("Matt, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| O | X | X |\n+---+---+---+\n| d | O | f |\n+---+---+---+\n| X | h | i |\n+---+---+---+");
+            writer.Received().WriteLine("Olly, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| O | X | X |\n+---+---+---+\n| d | O | f |\n+---+---+---+\n| X | h | O |\n+---+---+---+");
+            writer.Received().WriteLine("Winner Olly!");
         }
     }
 }

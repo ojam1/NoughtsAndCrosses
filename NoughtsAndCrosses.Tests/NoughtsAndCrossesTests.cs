f6746c2dbad15ace7313e406ad65029976fe269a
Oliver Ranson <Oliver.Ranson@firsthelpline.com>
2018-08-08 11:21:53 +0100

    Verifies a quick game can be played

--- NoughtsAndCrosses.Tests/NoughtsAndCrossesTests.cs
+++ NoughtsAndCrosses.Tests/NoughtsAndCrossesTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using NSubstitute;
 
@@ -5,23 +6,34 @@ namespace NoughtsAndCrosses.Tests
 {
     public class NoughtsAndCrossesTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private string _playerOne = "Player One";
+        private string _playerTwo = "Player Two";
 
         [Test]
-        public void Outputs_players_names()
+        public void Outputs_quick_game()
         {
             var writer = Substitute.For<IWriter>();
-            writer.ReadLine().Returns("Matt Olly");
+            writer.ReadLine().Returns(_playerOne, _playerTwo, "a", "d", "b", "e", "c");
 
             new Game(writer).Start();
             
-            writer.Received(3).WriteLine(Arg.Any<string>());
-            writer.Received().WriteLine("Enter players names with a space between them");
-            writer.Received().WriteLine("Hello Matt");
-            writer.Received().WriteLine("Hello Olly");
+            writer.Received(16).WriteLine(Arg.Any<string>());
+            writer.Received().WriteLine("Enter player one name");
+            writer.Received().WriteLine($"Hello {_playerOne}");
+            writer.Received().WriteLine("Enter player two name");
+            writer.Received().WriteLine($"Hello {_playerTwo}");
+            writer.Received().WriteLine("+---+---+---+\n| a | b | c |\n+---+---+---+\n| d | e | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine($"{_playerOne}, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| X | b | c |\n+---+---+---+\n| d | e | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine($"{_playerTwo}, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| X | b | c |\n+---+---+---+\n| O | e | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine($"{_playerOne}, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| X | X | c |\n+---+---+---+\n| O | e | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine($"{_playerTwo}, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| X | X | c |\n+---+---+---+\n| O | O | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine($"{_playerOne}, please enter your move");
+            writer.Received().WriteLine("+---+---+---+\n| X | X | X |\n+---+---+---+\n| O | O | f |\n+---+---+---+\n| g | h | i |\n+---+---+---+");
+            writer.Received().WriteLine($"Winner {_playerOne}!");
         }
     }
 }
